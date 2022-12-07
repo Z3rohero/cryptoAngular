54b363f03ae3d39442b304e4cd31e2603a64c963
Zero <Zero@hotmail.com>
2022-12-06 22:32:12 -0500

    cambios

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+xd
 # CryptoAngular
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.0.2.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+//para declar los datos que se encuentra el array
+interface Coin {
+  id: string;
+  name: string;
+  symbol: string;
+  image: string;
+  current_price:number;
+  price_change_percentage_24: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'cryptoAngular';
+export class AppComponent implements OnInit  {
+  constructor(private http: HttpClient) { }
+   
+  coins: [] = []
+  ngOnInit() {
+    this.http.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .subscribe((res) => {
+      console.log(res);
+  }
+  );
+   }
 }
